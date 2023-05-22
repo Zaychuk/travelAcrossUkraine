9eb68b4a8bc030f9fec3998ccc5fc29c99e62202
Taras Zaiats <taraszayats99@gmail.com>
2023-05-22 21:11:21 +0300

    fix: refactoring

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
 // Core
-export { default as GlobalStyles, AppContext, useGlobalStylesRoot } from './core/GlobalStyles'
-export { default as EnvironmentPanel } from './core/EnvironmentPanel'
 export { default as MissingContent } from './core/MissingContent'
 // Additional Components
 
